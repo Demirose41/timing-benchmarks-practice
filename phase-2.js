4b3cccb64d231b1af1cdfb3720cf1e06aa6c7904
demirose41 <demirose41@gmail.com>
2024-10-09 20:11:02 -0700

    2: completed phase 2

--- phase-2.js
+++ phase-2.js
@@ -2,14 +2,28 @@ const [addNums, addManyNums] = require("./phase-1");
 
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
-  // Fill this in
+  let total = 0;
+  let totals = [];
 
+  for(let i = increment; i <= 10 * increment; i += increment){
+    total = addNums(i);
+    totals = [...totals, total];
+  }
+
+  return totals;
 }
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
-  // Fill this in
+  let total = 0;
+  let totals = [];
 
+  for(let n = increment; n <= 10 * increment; n += increment){
+    total = addManyNums(n);
+    totals = [...totals, total];
+  }
+
+  return totals;
 }
 
 module.exports = [addNums10, addManyNums10];
